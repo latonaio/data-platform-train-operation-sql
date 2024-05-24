42cdf510925d7f90721d9977e5e70926c15195d7
misupopo <kokorononakaniitumo@yahoo.co.jp>
2024-05-24 11:20:17 +0900

    no message

--- data-platform-train-operation-sql-item-data.sql
+++ data-platform-train-operation-sql-item-data.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `data_platform_train_operation_item_data`
 (
   `RailwayLine`                  int(16) NOT NULL,
-  `TrainOperationVersion`        int(4) NOT NULL,
-  `WeekdayType`                  varchar(6) NOT NULL,
+  `OperationDate`                date NOT NULL,
   `TrainOperationID`             int(4) NOT NULL,
-  `ExpressType`                  varchar(4) NOT NULL,
   `RailwayLineStationID`         int(4) NOT NULL,
   `StopStation`                  int(16) NOT NULL,
-  `DepartingTime`                time(16) NOT NULL,
-  `ArrivingTime`                 time(16) DEFAULT NULL,
+  `PlannedArrivalTime`           time DEFAULT NULL,
+  `PlannedDepartureTime`         time NOT NULL,
+  `ActualArrivalTime`            time DEFAULT NULL,
+  `ActualDepartureTime`          time NOT NULL,
   `Description`                  varchar(60) NOT NULL,
   `OperationRemarks`             varchar(100) DEFAULT NULL,
   `CreationDate`                 date NOT NULL,
@@ -20,9 +20,9 @@ CREATE TABLE `data_platform_train_operation_item_data`
   `IsReleased`                   tinyint(1) DEFAULT NULL,
   `IsMarkedForDeletion`          tinyint(1) DEFAULT NULL,
 
-    PRIMARY KEY (`RailwayLine`, `TrainOperationVersion`, `WeekdayType`, `TrainOperationID`, `ExpressType`, `RailwayLineStationID`, `StopStation`),
+    PRIMARY KEY (`RailwayLine`, `OperationDate`, `TrainOperationID`, `RailwayLineStationID`),
 
-    CONSTRAINT `DPFMTrainOperationItemData_fk` FOREIGN KEY (`RailwayLine`, `TrainOperationVersion`, `WeekdayType`, `TrainOperationID`, `ExpressType`) REFERENCES `data_platform_train_operation_header_data` (`RailwayLine`, `TrainOperationVersion`, `WeekdayType`, `TrainOperationID`, `ExpressType`),
+    CONSTRAINT `DPFMTrainOperationItemData_fk` FOREIGN KEY (`RailwayLine`, `OperationDate`, `TrainOperationID`) REFERENCES `data_platform_train_operation_header_data` (`RailwayLine`, `OperationDate`, `TrainOperationID`),
     CONSTRAINT `DPFMTrainOperationItemDataStopStation_fk` FOREIGN KEY (`RailwayLine`, `RailwayLineStationID`, `StopStation`) REFERENCES `data_platform_railway_line_stop_station_data` (`RailwayLine`, `RailwayLineStationID`, `StopStation`),
     CONSTRAINT `DPFMTrainOperationItemDataCreateUser_fk` FOREIGN KEY (`CreateUser`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
     CONSTRAINT `DPFMTrainOperationItemDataLastChangeUser_fk` FOREIGN KEY (`LastChangeUser`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`)
